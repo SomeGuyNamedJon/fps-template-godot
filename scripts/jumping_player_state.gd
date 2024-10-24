72f9a98d8583fda3978e6ac2697e111b344f25ab
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2024-10-23 22:11:45 -0500

    added double jump

--- scripts/jumping_player_state.gd
+++ scripts/jumping_player_state.gd
@@ -4,10 +4,12 @@ class_name JumpingPlayerState extends PlayerMovementState
 @export var JUMP_VELOCITY: float = 4.5
 var entering_speed: float
 var next_state: String
+var double_jump: bool = false
 
 func enter(previous_state: State) -> void:
 	player.velocity.y += JUMP_VELOCITY
-	entering_speed = player.velocity.length() 
+	entering_speed = player.velocity.length()
+	next_state = default_state 
 	
 	if previous_state.name == "SlidingPlayerState":
 		next_state = default_state
@@ -15,6 +17,9 @@ func enter(previous_state: State) -> void:
 	else:
 		next_state = previous_state.name
 		animation_player.pause()
+		
+func exit() -> void:
+	double_jump = false
 
 func update(delta: float) -> void:
 	player.update_gravity(delta)
@@ -22,3 +27,7 @@ func update(delta: float) -> void:
 	
 	if player.is_on_floor():
 		transition.emit(next_state) 
+	
+	if not double_jump and Input.is_action_just_pressed("jump"):
+		player.velocity.y += JUMP_VELOCITY
+		double_jump = true
