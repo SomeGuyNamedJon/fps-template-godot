1bc72c32139fd53b2871153fb6a3cc98519e0ac0
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2024-11-02 19:38:11 -0500

    added idle weapon sway

--- scripts/init_weapon.gd
+++ scripts/init_weapon.gd
@@ -7,11 +7,21 @@ extends Node3D
 		WEAPON_TYPE = value
 		if Engine.is_editor_hint():
 			load_weapon()
+			
+@export var sway_noise: NoiseTexture2D
+@export var sway_speed: float = 1.2
+@export var reset: bool = false:
+	set(value):
+		reset = value
+		if Engine.is_editor_hint():
+			load_weapon()
 
 @onready var weapon_mesh: MeshInstance3D = %WeaponMesh
 @onready var shadow_mesh: MeshInstance3D = %ShadowMesh
 
 var mouse_movement: Vector2
+var random_sway: Vector2
+var time: float = 0.0
 
 func _ready() -> void:
 	load_weapon()
@@ -38,12 +48,30 @@ func load_weapon() -> void:
 	shadow_mesh.visible = WEAPON_TYPE.shadow
 	
 func sway_weapon(delta: float) -> void:
+	### Random sway
+	var sway_random: float = get_sway_noise()
+	var sway_random_adjusted: float = sway_random * WEAPON_TYPE.idle_sway_adjustment
+	# update time using delta and prepare our two random sway values using sine waves
+	time += delta * (sway_speed + sway_random)
+	random_sway.x = sin(time * 1.5 + sway_random_adjusted) / WEAPON_TYPE.random_sway_amount
+	random_sway.y = sin(time - sway_random_adjusted) / WEAPON_TYPE.random_sway_amount
+	
+	### Mouse movement sway
 	# clamp mouse movement
 	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min, WEAPON_TYPE.sway_max)
-	# adjust weapon position using lerp
-	position.x = lerp(position.x, WEAPON_TYPE.position.x - mouse_movement.x * WEAPON_TYPE.sway_amount_position * delta, WEAPON_TYPE.sway_speed_position)
-	position.y = lerp(position.y, WEAPON_TYPE.position.y + mouse_movement.y * WEAPON_TYPE.sway_amount_position * delta, WEAPON_TYPE.sway_speed_position)
-	# adjust weapon rotation using lerp
-	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y - mouse_movement.x * WEAPON_TYPE.sway_amount_rotation * delta, WEAPON_TYPE.sway_speed_rotation)
-	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + mouse_movement.y * WEAPON_TYPE.sway_amount_rotation * delta, WEAPON_TYPE.sway_speed_rotation)
 	
+	### Apply sway to weapon
+	# adjust weapon position using lerp
+	position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position + random_sway.x) * delta, WEAPON_TYPE.sway_speed_position)
+	position.y = lerp(position.y, WEAPON_TYPE.position.y + (mouse_movement.y * WEAPON_TYPE.sway_amount_position + random_sway.y) * delta, WEAPON_TYPE.sway_speed_position)
+	# adjust weapon rotation using lerp
+	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y - (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation + (random_sway.y * WEAPON_TYPE.idle_sway_rotation_strength)) * delta, WEAPON_TYPE.sway_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation + (random_sway.x * WEAPON_TYPE.idle_sway_rotation_strength)) * delta, WEAPON_TYPE.sway_speed_rotation)
+	
+func get_sway_noise() -> float:
+	var player_position: Vector3 = Vector3.ZERO
+	# only access global var in game to avoid warnings
+	if not Engine.is_editor_hint():
+		player_position = Global.player.global_position
+	# return noise value based on player position
+	return sway_noise.noise.get_noise_2d(player_position.x, player_position.y)
